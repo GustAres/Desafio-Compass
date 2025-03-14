3597e03c31f5e3fcda60d22c3cc8e07c7b368a11
Gustavo Procopio <rattesprocopio@gmail.com>
2025-03-14 16:27:10 -0300

    fix: Getter e Setter do Event

--- event-service/src/main/java/com/estagio/eventservice/model/Event.java
+++ event-service/src/main/java/com/estagio/eventservice/model/Event.java
@@ -3,19 +3,96 @@ package com.estagio.eventservice.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
-
 @Document(collection = "events")
 public class Event {
+    
     @Id
     private String id;
     private String eventName;
-    private LocalDateTime dateTime;
+    private String dateTime;
     private String cep;
     private String logradouro;
     private String bairro;
     private String cidade;
     private String uf;
 
-    // Getters e Setters
+
+    public Event() {
+    }
+
+
+    public Event(String eventName, String dateTime, String cep, String logradouro, String bairro, String cidade, String uf) {
+        this.eventName = eventName;
+        this.dateTime = dateTime;
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
 }
